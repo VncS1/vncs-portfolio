12b97635ac60aa435414bd68718a75854a4ea77b
Vinicius <62514933+VncS1@users.noreply.github.com>
2026-04-13 19:58:52 -0300

    fix:change nomenclature

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -181,7 +181,7 @@ export function Projects() {
                             d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
                           />
                         </svg>
-                        CODE
+                        Código
                       </a>
                     )}
                   </div>
