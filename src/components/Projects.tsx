0ac95d9bf711f46c184ba60eede71bb868d4ac92
Vinicius <machioni.dev@gmail.com>
2026-04-14 12:19:31 -0300

    fix: pucpcaldas link

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -34,7 +34,7 @@ export function Projects() {
       tags: ["FRONT-END", "UI/UX", "WEB"],
       image: "/projetos/puc.png",
       isOnWeb: true,
-      liveUrl: "www.pucpcaldas.br",
+      liveUrl: "https://www.pucpcaldas.br",
       isPrivate: true,
       codeUrl: "#",
     },
@@ -136,7 +136,7 @@ export function Projects() {
                             d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
                           />
                         </svg>
-                        LIVE
+                        Ver Website
                       </a>
                     ) : (
                       <></>
